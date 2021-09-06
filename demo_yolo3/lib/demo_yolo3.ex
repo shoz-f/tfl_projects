b182315fb97a55f834a6c6029e5d9f0a298d26f6
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2021-09-06 14:39:37 +0900

    revised

--- demo_yolo3/lib/demo_yolo3.ex
+++ demo_yolo3/lib/demo_yolo3.ex
@@ -3,8 +3,6 @@ defmodule DemoYolo3 do
   Documentation for `DemoYolo3`.
   """
 
-  alias DemoYolo3.Yolo3
-
   @doc """
   """
   def test() do
@@ -12,9 +10,10 @@ defmodule DemoYolo3 do
       CImg.create("test/dog.jpg")
       |> CImg.resize([416,416])
       |> CImg.to_flatnorm()
-    
-    TflInterp.set_input_tensor(Yolo3, 0, img.data)
-    TflInterp.invoke(Yolo3)
-    TflInterp.non_max_suppression_multi_class(Yolo3, 0, 1, 0.5, 0.25, 0.0)
+
+    DemoYolo3.Yolo3
+    |> TflInterp.set_input_tensor(0, img.data)
+    |> TflInterp.invoke()
+    |> TflInterp.non_max_suppression_multi_class(0, 1, 0.5, 0.25, 0.0)
   end
 end
