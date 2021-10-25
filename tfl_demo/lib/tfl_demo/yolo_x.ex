e43a2eac8d7fd4364f58f813851da6f26f5bc037
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2021-10-26 04:31:45 +0900

    add yolox postprocessing without nms

--- tfl_demo/lib/tfl_demo/yolo_x.ex
+++ tfl_demo/lib/tfl_demo/yolo_x.ex
@@ -3,65 +3,51 @@ defmodule TflDemo.YoloX.Prediction do
 end
 
 defmodule TflDemo.YoloX do
+  @moduledoc false
+
   def apply_yolox(img_file) do
     img = CImg.create(img_file)
-    
-    bin = CImg.dup(img) |> CImg.get_packed([640,640], 114) |> CImg.to_flatf4(true, true)
 
-    Npy.save("input.npy", struct(Npy, bin))
+    bin = CImg.dup(img)
+      |> CImg.get_packed([640,640], 114)
+      |> CImg.to_flatf4(true, true)
 
-    outputs = 
+    outputs =
       TflDemo.YoloX.Prediction
       |> TflInterp.set_input_tensor(0, bin.data)
       |> TflInterp.invoke()
       |> TflInterp.get_output_tensor(0)
+      |> Nx.from_binary({:f, 32})
+      |> Nx.reshape({8400, 85})
 
-#    %{
-#      descr: "<f4",
-#      shape: {1, 8400, 85},
-#      data: output
-#    }
-    Nx.from_binary(outputs, {:f, 32}) |> Nx.reshape({1, 8400, 85})
+    {boxes, scores} = reshape(outputs, {640, 640})
   end
-  
-  def postprocess(outputs, {hsize, wsize}) do
-     prms = Enum.map([8, 16, 32], fn x -> {div(hsize, x), div(wsize, x), x} end)
 
-     grid    = Enum.map(prms, &grid/1) |> Nx.concatenate(axis: 1)
-     strides = Enum.map(prms, &strides/1) |> Nx.concatenate(axis: 1)
+  def reshape(outputs, {hsize, wsize}) do
+     # resolution map
+     reso = Enum.map([8, 16, 32], fn x -> {Kernel.div(hsize, x), Kernel.div(wsize, x), x} end)
+
+     grid    = Enum.map(reso, &grid/1)    |> Nx.concatenate(axis: 0)
+     strides = Enum.map(reso, &strides/1) |> Nx.concatenate(axis: 0)
+
+     box_center = Nx.slice_axis(outputs, 0, 2, 1) |> Nx.add(grid)
+     box_wing   = Nx.slice_axis(outputs, 2, 2, 1) |> Nx.exp() |> Nx.divide(2)
 
      {
-       Nx.slice_axis(outputs, 0,  2, 2) |> Nx.add(grid) |> Nx.multiply(strides),
-       Nx.slice_axis(outputs, 2,  2, 2) |> Nx.exp()     |> Nx.multiply(strides),
-       Nx.slice_axis(outputs, 4,  1, 2),
-       Nx.slice_axis(outputs, 5, 80, 2)
+       # boxes
+       [Nx.subtract(box_center, box_wing), Nx.add(box_center, box_wing)] |> Nx.concatenate(axis: 1) |> Nx.multiply(strides),
+       # scores
+       Nx.multiply(Nx.slice_axis(outputs, 4, 1, 1), Nx.slice_axis(outputs, 5, 80, 1))
      }
   end
-  
+
   defp grid({hsize, wsize, _}) do
     xv = Nx.iota({wsize}) |> Nx.tile([hsize, 1])
     yv = Nx.iota({hsize}) |> Nx.tile([wsize, 1]) |> Nx.transpose
-    Nx.stack([xv, yv], axis: 2) |> Nx.reshape({1, :auto, 2})
+    Nx.stack([xv, yv], axis: 2) |> Nx.reshape({:auto, 2})
   end
   
   defp strides({hsize, wsize, stride}) do
-    Nx.tensor(stride) |> Nx.tile([1, hsize*wsize, 1])
+    Nx.tensor(stride) |> Nx.tile([hsize*wsize, 1])
   end
-
-  def apply_yolox2(npy_file) do
-    {:ok, npy} = Npy.load(npy_file)
-
-    output = 
-      TflDemo.YoloX.Prediction
-      |> TflInterp.set_input_tensor(0, npy.data)
-      |> TflInterp.invoke()
-      |> TflInterp.get_output_tensor(0)
-
-    %{
-      descr: "<f4",
-      shape: {1, 8400, 85},
-      data: output
-    }
-  end
-
 end
