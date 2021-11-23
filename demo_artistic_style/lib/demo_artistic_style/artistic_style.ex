e3b9626481a96d7b1543d19d0fc598448605ff32
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2021-11-24 01:01:51 +0900

    remove old demo_artistic

--- demo_artistic_style/lib/demo_artistic_style/artistic_style.ex
+++
@@ -1,7 +0,0 @@
-defmodule DemoArtisticStyle.Prediction do
-  use TflInterp, model: "priv/magenta_arbitrary-image-stylization-v1-256_int8_prediction_1.tflite"
-end
-
-defmodule DemoArtisticStyle.Transfer do
-  use TflInterp, model: "priv/magenta_arbitrary-image-stylization-v1-256_int8_transfer_1.tflite"
-end
--- demo_artistic_style/test/demo_artistic_style_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule DemoArtisticStyleTest do
-  use ExUnit.Case
-  doctest DemoArtisticStyle
-
-  test "greets the world" do
-    assert DemoArtisticStyle.hello() == :world
-  end
-end
